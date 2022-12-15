702d96cdde46b9779907267843705043933d5a06
Atom <derickkpinheiro@gmail.com>
2022-12-15 13:12:46 -0300

    Add new anomalies

--- phoenix-data/src/anomalies.rs
+++ phoenix-data/src/anomalies.rs
@@ -1,23 +1,29 @@
 use std::fmt::Display;
 
-use rand::{Rng, rngs::ThreadRng};
+use rand::{rngs::ThreadRng, Rng};
 
-use crate::{Stat, Emoji};
+use crate::{Emoji, Stat};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum AnomalyType {
-    Guardian,
+    Treant,
     Orc,
+    Vexbug,
+    Guardian,
     Ferak,
     Oozeling,
     Nightfall,
+    Timberwraith,
 }
 
 impl AnomalyType {
     const fn name(&self) -> &'static str {
         match self {
-            AnomalyType::Guardian => "Guardião",
+            AnomalyType::Treant => "Treant",
             AnomalyType::Orc => "Orc",
+            AnomalyType::Vexbug => "Vexbug",
+            AnomalyType::Timberwraith => "Timberwraith",
+            AnomalyType::Guardian => "Guardião",
             AnomalyType::Ferak => "Ferak",
             AnomalyType::Oozeling => "Oozeling",
             AnomalyType::Nightfall => "Nightfall",
@@ -26,11 +32,14 @@ impl AnomalyType {
 
     const fn image(&self) -> &'static str {
         match self {
-            AnomalyType::Guardian => "https://i.imgur.com/wWkkOJX.png",
+            AnomalyType::Treant => "https://i.imgur.com/QuWuU0j.png",
             AnomalyType::Orc => "https://i.imgur.com/wIfjahq.png",
+            AnomalyType::Vexbug => "https://i.imgur.com/PWpbL8r.png",
+            AnomalyType::Guardian => "https://i.imgur.com/wWkkOJX.png",
             AnomalyType::Ferak => "https://i.imgur.com/sNnkwxi.png",
             AnomalyType::Oozeling => "https://i.imgur.com/hro2X3W.png",
-            AnomalyType::Nightfall => "https://i.imgur.com/ddX6CCu.png"
+            AnomalyType::Nightfall => "https://i.imgur.com/ddX6CCu.png",
+            AnomalyType::Timberwraith => "https://i.imgur.com/w6xnlJf.png",
         }
     }
 }
@@ -54,12 +63,14 @@ pub struct AnomalyDefinition {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct AnomalyDrops {
     pub xp: i32,
-    pub gold: i32
+    pub gold: i32,
 }
 
 impl Display for AnomalyDrops {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} **Ouro**: {}\n{} **XP**: {}",
+        write!(
+            f,
+            "{} **Ouro**: {}\n{} **XP**: {}",
             Emoji::Gold,
             self.gold,
             Emoji::Experience,
@@ -79,7 +90,7 @@ pub struct Anomaly {
     pub agility: i32,
     pub intelligence: i32,
     pub level: i32,
-    pub rewards: AnomalyDrops
+    pub rewards: AnomalyDrops,
 }
 
 impl Anomaly {
@@ -96,6 +107,15 @@ impl Anomaly {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+pub const TREANT: AnomalyDefinition = AnomalyDefinition {
+    anomaly_type: AnomalyType::Treant,
+    health: Stat::new(50),
+    mana: Stat::new(10),
+    strength: 10,
+    agility: 1,
+    intelligence: 3,
+};
+
 pub const ORC: AnomalyDefinition = AnomalyDefinition {
     anomaly_type: AnomalyType::Orc,
     health: Stat::new(80),
@@ -103,6 +123,15 @@ pub const ORC: AnomalyDefinition = AnomalyDefinition {
     strength: 20,
     agility: 5,
     intelligence: 3,
+};
+
+pub const VEXBUG: AnomalyDefinition = AnomalyDefinition {
+    anomaly_type: AnomalyType::Vexbug,
+    health: Stat::new(60),
+    mana: Stat::new(15),
+    strength: 10,
+    agility: 3,
+    intelligence: 15,
 };
 
 pub const GUARDIAN: AnomalyDefinition = AnomalyDefinition {
@@ -141,35 +170,67 @@ pub const NIGHTFALL: AnomalyDefinition = AnomalyDefinition {
     intelligence: 10,
 };
 
-pub const ANOMALIES: [AnomalyDefinition; 5] = [ORC, GUARDIAN, FERAK, OOZELING, NIGHTFALL];
+pub const TIMBERWRAITH: AnomalyDefinition = AnomalyDefinition {
+    anomaly_type: AnomalyType::Timberwraith,
+    health: Stat::new(225),
+    mana: Stat::new(50),
+    strength: 20,
+    agility: 15,
+    intelligence: 10,
+};
+
+pub const ANOMALIES: [AnomalyDefinition; 8] = [
+    TREANT,
+    ORC,
+    VEXBUG,
+    GUARDIAN,
+    FERAK,
+    OOZELING,
+    NIGHTFALL,
+    TIMBERWRAITH,
+];
 
 pub fn factor(rng: &mut ThreadRng, level: i32, extra_factor: f32) -> f32 {
     let level = level + 1;
     (level + (level / 2)) as f32 * 0.5 * rng.gen_range(0.3..1.25) * (extra_factor / 2.0)
 }
 
-pub fn calculate_potency(rng: &mut ThreadRng, health: f32, mana: f32, strength: f32, agility: f32, intelligence: f32) -> f32 {
-    (health * 1.1 + mana * 1.1 + (strength + strength / 1.5f32)) + (agility + intelligence + rng.gen_range(1f32..5f32)) / rng.gen_range(6.85..8.0)
+pub fn calculate_potency(
+    rng: &mut ThreadRng,
+    health: f32,
+    mana: f32,
+    strength: f32,
+    agility: f32,
+    intelligence: f32,
+) -> f32 {
+    (health * 1.1 + mana * 1.1 + (strength + strength / 1.5f32))
+        + (agility + intelligence + rng.gen_range(1f32..5f32)) / rng.gen_range(6.85..8.0)
 }
 
-
 pub fn generate_random_anomaly(player_level: i32) -> Anomaly {
-    let mut rng = rand::thread_rng();
+    let rng = &mut rand::thread_rng();
     let random_index = rng.gen_range(0..ANOMALIES.len());
     let def = ANOMALIES[random_index];
 
-    let level = ((player_level + 1) as f32 * rng.gen_range(0.7..1.3)) as i32;
-    let health = Stat::new((factor(&mut rng, level, 1.3) * def.health.max as f32) as i32);
-    let mana = Stat::new((factor(&mut rng, level, 1.2) * def.mana.max as f32) as i32);
-    let strength = (factor(&mut rng, level, 1.3) * def.strength as f32) as i32;
-    let agility = (factor(&mut rng, level, 1.25) * def.agility as f32) as i32;
-    let intelligence = (factor(&mut rng, level, 1.0) * def.intelligence as f32) as i32;
+    let level = (player_level as f32 * rng.gen_range(0.6..1.3)).max(1.0) as i32;
+    let health = Stat::new((factor(rng, level, 1.3) * def.health.max as f32) as i32);
+    let mana = Stat::new((factor(rng, level, 1.2) * def.mana.max as f32) as i32);
+    let strength = (factor(rng, level, 1.3) * def.strength as f32) as i32;
+    let agility = (factor(rng, level, 1.25) * def.agility as f32) as i32;
+    let intelligence = (factor(rng, level, 1.0) * def.intelligence as f32) as i32;
 
-    let potency = calculate_potency(&mut rng, health.max as f32, mana.max as f32, strength as f32, agility as f32, intelligence as f32);
+    let potency = calculate_potency(
+        rng,
+        health.max as f32,
+        mana.max as f32,
+        strength as f32,
+        agility as f32,
+        intelligence as f32,
+    );
     let value = ((potency * rng.gen_range(0.9..1.1)) as i32).max(1);
 
-    let xp_reward = rng.gen_range(3..7) * level * (value / 5);
-    let gold_reward = rng.gen_range(1..3) * level * (value / 9);
+    let xp_reward = rng.gen_range(5..7) * level * (value / 5);
+    let gold_reward = rng.gen_range(2..3) * level * (value / 9);
 
     Anomaly {
         definition: def,
@@ -181,6 +242,9 @@ pub fn generate_random_anomaly(player_level: i32) -> Anomaly {
         agility: agility.max(1),
         intelligence: intelligence.max(1),
         level: level.max(1),
-        rewards: AnomalyDrops { xp: xp_reward, gold: gold_reward }
+        rewards: AnomalyDrops {
+            xp: xp_reward,
+            gold: gold_reward,
+        },
     }
 }
